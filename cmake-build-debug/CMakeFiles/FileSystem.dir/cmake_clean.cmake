9141e2a50cc4e98750ab8cfb628e22557399e44c
AlexMigl03 <alessandro.migliaccio.mgl@gmail.com>
2023-05-31 19:02:03 +0200

    Aggiunto Menu e funzioni menu

---
+++ cmake-build-debug/CMakeFiles/FileSystem.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FileSystem.dir/filesystem.c.obj"
+  "CMakeFiles/FileSystem.dir/filesystem.c.obj.d"
+  "CMakeFiles/FileSystem.dir/main.c.obj"
+  "CMakeFiles/FileSystem.dir/main.c.obj.d"
+  "FileSystem.exe"
+  "FileSystem.exe.manifest"
+  "FileSystem.pdb"
+  "libFileSystem.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FileSystem.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
